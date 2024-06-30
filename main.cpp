94c873b8144e18c0c48128dbc955560fa450927f
JAVIER <javiermonjesnuevo@gmail.com>
2024-06-30 12:23:00 -0600

    Se modifican archivos para corregir archivos json

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <iostream>
+#include <string>
+#include "json.hpp"
+
+
+using json = nlohmann::json;
+
+
+
+int main() {
+    std::cout << "Hola Mundo" << std::endl;
+    std::cout << "Hola Mundo" << std::endl;
+    std::cout << "Hola Mundo" << std::endl;
+    return 0;
+}
