e5602e73d2fdad4eccf485394cad5e857666cf06
JAVIER <javiermonjesnuevo@gmail.com>
2024-07-01 19:54:19 -0600

    Se agregan arbol b y bb

---
+++ ArbolB.h
@@ -0,0 +1,187 @@
+#ifndef ARBOLB_H_INCLUDED
+#define ARBOLB_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+using namespace std;
+
+#include "Pagina.h"
+#include "ListaCircularDoble.h"
+
+class ArbolB
+{
+private:
+  Pagina *raiz;
+
+  void insertarNoLleno(Pagina *nodo, const Avion &avion)
+  {
+    int i = nodo->aviones.size() - 1;
+
+    if (nodo->esHoja)
+    {
+      nodo->aviones.push_back(avion);
+      while (i >= 0 && nodo->aviones[i].getClave() > avion.getClave())
+      {
+        nodo->aviones[i + 1] = nodo->aviones[i];
+        i--;
+      }
+      nodo->aviones[i + 1] = avion;
+    }
+    else
+    {
+      while (i >= 0 && nodo->aviones[i].getClave() > avion.getClave())
+      {
+        i--;
+      }
+      i++;
+      if (nodo->hijos[i]->aviones.size() == 4)
+      {
+        dividirHijo(nodo, i);
+        if (nodo->aviones[i].getClave() < avion.getClave())
+        {
+          i++;
+        }
+      }
+      insertarNoLleno(nodo->hijos[i], avion);
+    }
+  }
+
+  void dividirHijo(Pagina *nodo, int i)
+  {
+    Pagina *y = nodo->hijos[i];
+    Pagina *z = new Pagina(y->esHoja);
+
+    nodo->hijos.insert(nodo->hijos.begin() + i + 1, z);
+    nodo->aviones.insert(nodo->aviones.begin() + i, y->aviones[2]);
+
+    z->aviones.push_back(y->aviones[3]);
+    y->aviones.resize(2);
+
+    if (!y->esHoja)
+    {
+      for (int j = 0; j < 3; j++)
+      {
+        z->hijos.push_back(y->hijos[j + 2]);
+      }
+      y->hijos.resize(2);
+    }
+  }
+
+  void generarDot(Pagina *nodo, ofstream &archivo, int &contador)
+  {
+    int nodoID = contador++;
+    archivo << "Nodo" << nodoID << " [label=\"";
+    for (const auto &avion : nodo->aviones)
+    {
+      archivo << " Avion: " << avion.numeroRegistro << " - ";
+      archivo << avion.vuelo << " - ";
+      archivo << avion.estado << " |";
+    }
+    archivo << "\"];\n";
+
+    if (!nodo->esHoja)
+    {
+      for (size_t i = 0; i < nodo->hijos.size(); ++i)
+      {
+        int hijoID = contador;
+        generarDot(nodo->hijos[i], archivo, contador);
+        archivo << "Nodo" << nodoID << " -> Nodo" << hijoID << ";\n";
+      }
+    }
+  }
+
+  string buscar(Pagina *pagina, const string &numeroVuelo)
+  {
+    int i = 0;
+    while (i < static_cast<int>(pagina->aviones.size()))
+    {
+      if (i < static_cast<int>(pagina->aviones.size()) && numeroVuelo == pagina->aviones[i].getNumeroVuelo())
+      {
+        return pagina->aviones[i].getCiudadDestino();
+      }
+      i++;
+    }
+
+    if (!pagina->esHoja)
+    {
+      for (Pagina *hijo : pagina->hijos)
+      {
+        string resultado = buscar(hijo, numeroVuelo);
+        if (!resultado.empty())
+        {
+          return resultado;
+        }
+      }
+    }
+    return "";
+  }
+
+public:
+  ArbolB() : raiz(nullptr) {}
+
+  void insertar(Avion &avion)
+  {
+    if (!raiz)
+    {
+      raiz = new Pagina(true);
+      raiz->aviones.push_back(avion);
+    }
+    else
+    {
+      if (raiz->aviones.size() == 4)
+      {
+        Pagina *s = new Pagina(false);
+        s->hijos.push_back(raiz);
+        dividirHijo(s, 0);
+        insertarNoLleno(s, avion);
+        raiz = s;
+      }
+      else
+      {
+        insertarNoLleno(raiz, avion);
+      }
+    }
+  }
+
+  string buscarAvion(const string &numeroVuelo)
+  {
+    string ciudad = "";
+    if (raiz == nullptr)
+    {
+      return "";
+    }
+    ciudad = buscar(raiz, numeroVuelo);
+    return ciudad;
+  }
+
+  void generarReporte()
+  {
+    ofstream archivo;
+    archivo.open("reporteDisponibles.dot", ios::out);
+    if (archivo.is_open())
+    {
+      archivo << "digraph G {\n";
+      archivo << "fontname=\"Helvetica,Arial,sans-serif\"\n";
+      archivo << "node [fontname=\"Helvetica,Arial,sans-serif\", style=filled, fillcolor=lightgrey, fontcolor=black];\n";
+      archivo << "edge [fontname=\"Helvetica,Arial,sans-serif\", color=black];\n";
+      int contador = 0;
+      if (raiz != nullptr)
+      {
+        generarDot(raiz, archivo, contador);
+      }
+    archivo << "labelloc=\"t\";\n"; // Localización del título (top)
+    archivo << "label=\"Reporte de Disponibles\";\n"; // Título del gráfico
+
+      archivo << "}\n";
+      archivo.close();
+      system("dot -Tpng reporteDisponibles.dot -o reporteDisponibles.png");
+      system("reporteDisponibles.png");
+    }
+    else
+    {
+      cerr << "No se pudo abrir el archivo para escribir\n";
+    }
+  }
+};
+
+#endif // ARBOLB_H_INCLUDED
---
+++ ArbolBinarioBusqueda.h
@@ -0,0 +1,264 @@
+#ifndef ARBOLBINARIOBUSQUEDA_H_INCLUDED
+#define ARBOLBINARIOBUSQUEDA_H_INCLUDED
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+#include <cstdlib>
+using namespace std;
+
+#include "Pilotos.h"
+#include "NodoBinarioBusqueda.h"
+
+class ArbolBB
+{
+private:
+  NodoBB *raiz;
+  ofstream archivo;
+  string nodoDato;
+
+public:
+  int recorrido = 0;
+  ArbolBB();
+  bool estaVacio();
+
+  void insertar(Piloto *dato);
+  NodoBB *insertarNodo(Piloto *dato, NodoBB *nodoPtr);
+
+  void eliminar(string numeroId);
+  NodoBB *eliminarNodo(string numeroId, NodoBB *nodoPtr);
+
+  string buscar(string numeroId);
+  string buscarNodo(string numeroId, NodoBB *nodoPtr);
+  NodoBB *buscarMin(NodoBB *nodoPtr);
+
+  void RecorridoIn();
+  void RecorridoIn(NodoBB *nodoPtr);
+  void RecorridoPre();
+  void RecorridoPre(NodoBB *nodoPtr);
+  void RecorridoPost();
+  void RecorridoPost(NodoBB *nodoPtr);
+
+  void generarReporte();
+  void imprimirNodo(NodoBB *nodoPtr);
+  ~ArbolBB();
+};
+
+ArbolBB::ArbolBB()
+{
+  raiz = nullptr;
+}
+
+bool ArbolBB::estaVacio()
+{
+  return (raiz == nullptr);
+}
+
+void ArbolBB::insertar(Piloto *dato)
+{
+  raiz = insertarNodo(dato, raiz);
+}
+
+NodoBB *ArbolBB::insertarNodo(Piloto *dato, NodoBB *nodoPtr)
+{
+  if (nodoPtr == nullptr)
+  {
+    NodoBB *nuevo = new NodoBB(dato);
+    nodoPtr = nuevo;
+  }
+  else if (dato->horasVuelo < nodoPtr->getHorasVuelo())
+  {
+    nodoPtr->setIzq(insertarNodo(dato, nodoPtr->getIzq()));
+  }
+  else if (dato->horasVuelo > nodoPtr->getHorasVuelo())
+  {
+    nodoPtr->setDer(insertarNodo(dato, nodoPtr->getDer()));
+  }
+  else
+  {
+    cout << "Nodo duplicado\n";
+  }
+  return nodoPtr;
+}
+
+void ArbolBB::eliminar(string numeroId)
+{
+  raiz = eliminarNodo(numeroId, raiz);
+}
+
+NodoBB *ArbolBB::eliminarNodo(string numeroId, NodoBB *nodoPtr)
+{
+  if (nodoPtr == nullptr)
+    return nodoPtr;
+
+  if (numeroId < nodoPtr->getNumeroId())
+  {
+    nodoPtr->setIzq(eliminarNodo(numeroId, nodoPtr->getIzq()));
+  }
+  else if (numeroId > nodoPtr->getNumeroId())
+  {
+    nodoPtr->setDer(eliminarNodo(numeroId, nodoPtr->getDer()));
+  }
+  else
+  {
+    if (nodoPtr->getIzq() == nullptr && nodoPtr->getDer() == nullptr)
+    {
+      delete nodoPtr;
+      nodoPtr = nullptr;
+    }
+    else if (nodoPtr->getIzq() == nullptr)
+    {
+      NodoBB *temp = nodoPtr;
+      nodoPtr = nodoPtr->getDer();
+      delete temp;
+    }
+    else if (nodoPtr->getDer() == nullptr)
+    {
+      NodoBB *temp = nodoPtr;
+      nodoPtr = nodoPtr->getIzq();
+      delete temp;
+    }
+    else
+    {
+      NodoBB *temp = buscarMin(nodoPtr->getDer());
+      nodoPtr->setNumeroId(temp->getNumeroId());
+      nodoPtr->setDer(eliminarNodo(temp->getNumeroId(), nodoPtr->getDer()));
+    }
+  }
+  return nodoPtr;
+}
+
+string ArbolBB::buscar(string numeroId)
+{
+  string vuelo = buscarNodo(numeroId, raiz);
+  return vuelo;
+}
+
+string ArbolBB::buscarNodo(string numeroId, NodoBB *nodoPtr)
+{
+  if (nodoPtr == nullptr)
+  {
+    return "";
+  }
+  if (numeroId == nodoPtr->getNumeroId())
+  {
+    return nodoPtr->getVuelo();
+  }
+  else if (numeroId < nodoPtr->getNumeroId())
+  {
+    return buscarNodo(numeroId, nodoPtr->getIzq());
+  }
+  else
+  {
+    return buscarNodo(numeroId, nodoPtr->getDer());
+  }
+}
+
+NodoBB *ArbolBB::buscarMin(NodoBB *nodoPtr)
+{
+  while (nodoPtr->getIzq() != nullptr)
+  {
+    nodoPtr = nodoPtr->getIzq();
+  }
+  return nodoPtr;
+}
+
+void ArbolBB::RecorridoIn()
+{
+  RecorridoIn(raiz);
+  cout << endl;
+}
+
+void ArbolBB::RecorridoIn(NodoBB *nodoPtr)
+{
+  if (nodoPtr != nullptr)
+  {
+    RecorridoIn(nodoPtr->getIzq());
+    cout << nodoPtr->getHorasVuelo() << " -> ";
+    RecorridoIn(nodoPtr->getDer());
+  }
+}
+
+void ArbolBB::RecorridoPre()
+{
+  RecorridoPre(raiz);
+  cout << endl;
+}
+
+void ArbolBB::RecorridoPre(NodoBB *nodoPtr)
+{
+  if (nodoPtr != nullptr)
+  {
+    cout << nodoPtr->getHorasVuelo() << " -> ";
+    RecorridoPre(nodoPtr->getIzq());
+    RecorridoPre(nodoPtr->getDer());
+  }
+}
+
+void ArbolBB::RecorridoPost()
+{
+  RecorridoPost(raiz);
+  cout << endl;
+}
+
+void ArbolBB::RecorridoPost(NodoBB *nodoPtr)
+{
+  if (nodoPtr != nullptr)
+  {
+    RecorridoPost(nodoPtr->getIzq());
+    RecorridoPost(nodoPtr->getDer());
+    cout << nodoPtr->getHorasVuelo() << " -> ";
+  }
+}
+
+void ArbolBB::generarReporte() {
+    if (!ArbolBB::estaVacio()) {
+        archivo.open("reporteArbolBinariobusqueda.dot", ios::out);
+        archivo << "digraph G {\n"
+                << "  node [fontname=\"Helvetica,Arial,sans-serif\", style=filled, color=lightblue];\n"
+                << "  edge [fontname=\"Helvetica,Arial,sans-serif\"];\n\n";
+
+        imprimirNodo(raiz);
+        archivo << "labelloc=\"t\";\n"; // Localización del título (top)
+        archivo << "label=\"Reporte de Arbol Binario\";\n"; // Título del gráfico
+
+        archivo << "\n}";
+        archivo.close();
+        system("dot -Tpng reporteArbolBinariobusqueda.dot -o reporteArbolBinariobusqueda.png");
+        system("reporteArbolBinariobusqueda.png");
+    }
+}
+
+void ArbolBB::imprimirNodo(NodoBB *nodoPtr)
+{
+  if (nodoPtr == nullptr)
+  {
+    return;
+  }
+  if (nodoPtr->getIzq() != nullptr)
+  {
+    nodoDato = nodoPtr->graficarDatosArbolBinario();
+    archivo << nodoDato;
+    archivo << " -> ";
+    nodoDato = nodoPtr->getIzq()->graficarDatosArbolBinario();
+    archivo << nodoDato;
+    archivo << ";";
+  }
+  imprimirNodo(nodoPtr->getIzq());
+  if (nodoPtr->getDer() != nullptr)
+  {
+    nodoDato = nodoPtr->graficarDatosArbolBinario();
+    archivo << nodoDato;
+    archivo << " -> ";
+    nodoDato = nodoPtr->getDer()->graficarDatosArbolBinario();
+    archivo << nodoDato;
+    archivo << ";";
+  }
+  imprimirNodo(nodoPtr->getDer());
+}
+
+ArbolBB::~ArbolBB()
+{
+}
+
+#endif // ARBOLBINARIOBUSQUEDA_H_INCLUDED
